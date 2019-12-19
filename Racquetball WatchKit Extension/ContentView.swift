9dcc94dfb4ace2c316c0b6d2c83506659ceeffbe
Austin Conlon <austinconlon@icloud.com>
2019-12-18 23:00:32 -0800

    Initial Commit

---
+++ Racquetball WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Racquetball WatchKit Extension
+//
+//  Created by Austin Conlon on 12/18/19.
+//  Copyright © 2019 Austin Conlon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Racquetball WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Racquetball WatchKit Extension
+//
+//  Created by Austin Conlon on 12/18/19.
+//  Copyright © 2019 Austin Conlon. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
