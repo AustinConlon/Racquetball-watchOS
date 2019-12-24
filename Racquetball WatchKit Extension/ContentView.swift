2f7d6b81dfd64e73fd8e7b142a3bc508d6fd28e9
Austin Conlon <austinconlon@icloud.com>
2019-12-24 05:02:28 -0800

    implement model

--- Racquetball WatchKit Extension/ContentView.swift
+++ Racquetball WatchKit Extension/ContentView.swift
@@ -9,13 +9,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var match = Match()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Points")
+            
+            HStack() {
+                Group {
+                    Button(action: {
+                        self.match.winRally(for: Team.one)
+                    }) {
+                        Image(systemName: "\(match.teamGameScores[0]).square")
+                        .font(.largeTitle)
+                    }
+                }
+                
+                Group {
+                    Button(action: {
+                        self.match.winRally(for: Team.two)
+                    }) {
+                        Image(systemName: "\(match.teamGameScores[1]).square")
+                        .font(.largeTitle)
+                    }
+                }
+            }
+                
+            Text("Points")
+            
+            HStack() {
+                Group {
+                    Button(action: {
+                        self.match.winRally(for: Team.one)
+                    }) {
+                        Image(systemName: "\(match.teamGameScores[0]).square")
+                        .font(.largeTitle)
+                    }
+                }
+                
+                Group {
+                    Button(action: {
+                        self.match.winRally(for: Team.two)
+                    }) {
+                        Image(systemName: "\(match.teamGameScores[1]).square")
+                        .font(.largeTitle)
+                    }
+                }
+            }
+            .navigationBarTitle(match.server == Team.one ? "You Serve" : "You Receive")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            .previewDevice("Apple Watch Series 5 - 44mm")
+            ContentView()
+            .previewDevice("Apple Watch Series 5 - 40mm")
+            ContentView()
+            .previewDevice("Apple Watch Series 3 - 42mm")
+            ContentView()
+            .previewDevice("Apple Watch Series 3 - 38mm")
+        }
     }
 }
